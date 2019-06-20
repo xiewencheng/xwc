917bf609c1a99a8c62ac3d3924ac2a15517bd4fe
unknown <1015790608@qq.com>
2019-06-20 17:29:17 +0800

    first commit

---
+++ resources/views/class/add.blade.php
@@ -0,0 +1,31 @@
+{{--<!doctype html>--}}
+{{--<html lang="en">--}}
+{{--<head>--}}
+{{--    <meta charset="UTF-8">--}}
+{{--    <meta name="viewport"--}}
+{{--          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">--}}
+{{--    <meta http-equiv="X-UA-Compatible" content="ie=edge">--}}
+{{--    <title>Document</title>--}}
+{{--</head>--}}
+{{--<body>--}}
+{{--<form action="class">--}}
+{{--    <table>--}}
+{{--        <tr>--}}
+{{--            <td>Id</td>--}}
+{{--            <td><input type="text" name="id" id="id"></td>--}}
+{{--        </tr>--}}
+{{--        <tr>--}}
+{{--            <td>班级名称</td>--}}
+{{--            <td><input type="text" name="name" id="name"></td>--}}
+{{--        </tr>--}}
+{{--        <tr>--}}
+{{--            <td>总人数</td>--}}
+{{--            <td><input type="text" name="sum" id="sum"></td>--}}
+{{--        </tr>--}}
+{{--        <tr>--}}
+{{--            <td><input type="submit" value="录入"></td>--}}
+{{--        </tr>--}}
+{{--    </table>--}}
+{{--</form>--}}
+{{--</body>--}}
+{{--</html>--}}
